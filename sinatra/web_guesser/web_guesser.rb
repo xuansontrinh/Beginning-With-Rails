2e6fde3b690c1e3ea39858b6c49b2ef860a4cc24
Son Trinh <xuanson.trinh@outlook.com>
2017-11-08 13:56:39 +0700

    - Modified a bit

--- sinatra/web_guesser/web_guesser.rb
+++ sinatra/web_guesser/web_guesser.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
 SECRET_NUMBER = rand(100)
+@@n_guesses = 5
 def check_guess guess
+  flag = false
   if guess <= SECRET_NUMBER - 5
     message = "Way to low!"
   elsif guess < SECRET_NUMBER
@@ -12,10 +14,23 @@ def check_guess guess
     message = "A little high!"
   else
     message = "Correct!"
+    flag = true
   end
+  return message, flag
 end
 get '/' do
   guess = (params['guess']).to_i
-  message = check_guess(guess)
-  erb :index, :locals => {:numby => SECRET_NUMBER, :message => message, :guess => guess}
+  message, flag = check_guess(guess)
+  if (flag == false && @@n_guesses == 0)
+    @@n_guesses = 5
+    message += " Out of turn, the game now resets the new number!"
+    SECRET_NUMBER = rand(100)
+  elsif flag == true
+    @@n_guesses = 5
+    message += " New number will be set!"
+    SECRET_NUMBER = rand(100)
+  else
+    @@n_guesses -= 1
+  end
+  erb :index, :locals => {:numby => SECRET_NUMBER, :message => message, :guess => guess, :cheat => params['cheat']}
 end
