594fc2137fd8177f12c42d00294602bb4f99460c
Son Trinh <xuanson.trinh@outlook.com>
2017-11-09 14:08:25 +0700

    Preparing to deploy first sinatra app.

--- sinatra/songs_by_sinatra/main.rb
+++ sinatra/songs_by_sinatra/main.rb
@@ -1,27 +1,58 @@
-require './song'
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/reloader' if development?
 require 'sass'
 require 'slim'
+require 'byebug'
+require './song'
 
 get('/styles.css'){ scss :styles }
+
+configure do
+enable :sessions
+set :username, 'frank' set :password, 'sinatra'
+end
+
+get '/set/:name' do
+  session[:name] = params[:name]
+end
+
+get '/get/hello' do
+  "Hello #{session[:name]}"
+end
 
 get '/' do
   @name = "Son"
   @title = "Jump Start Sinatra"
-  erb :home
+  slim :home
 end
 
 get '/about' do
   @title = "All About This Website"
-  erb :about
+  slim :about
 end
 
 get '/contact' do
   @title = "Jump Start Sinatra"
-  erb :contact
+  slim :contact
 end
 
 not_found do
-  erb :not_found
+  @title = "The Link Does Not Exist!"
+  slim :not_found
+end
+
+get '/login' do
+  slim :login
+end
+
+post '/login' do
+  if params[:username] == settings.username && params[:password] == settings.password session[:admin] = true redirect to('/songs')
+  else
+    slim :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect to('/login')
 end
--- sinatra/songs_by_sinatra/song.rb
+++ sinatra/songs_by_sinatra/song.rb
@@ -1,7 +1,9 @@
 require 'dm-core'
 require 'dm-migrations'
 
-DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+configure do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
 
 class Song
   include DataMapper::Resource
@@ -10,11 +12,51 @@ class Song
   property :lyrics, Text
   property :length, Integer
   property :released_on, Date
+
+  def released_on=date
+    super Date.strptime(date, '%m/%d/%Y')
+  end
 end
 
 DataMapper.finalize
 
-get '/songs' do
+get '/songs/?' do
+  @title = "All Songs By Sinatra"
   @songs = Song.all
   slim :songs
+end
+
+post '/songs' do
+  @title = "All Songs By Sinatra"
+  song = Song.create(params[:song])
+  redirect to("/songs/#{song.id}")
+end
+
+get '/songs/new' do
+  halt(401,'Not Authorized') unless session[:admin]
+  @title = "Add New Song By Sinatra"
+  @song = Song.new
+  slim :new_song
+end
+
+get '/songs/:id' do
+  @title = "Specific Song By Sinatra"
+  @song = Song.get(params[:id])
+  slim :show_song
+end
+
+get '/songs/:id/edit' do
+  @song = Song.get(params[:id])
+  slim :edit_song
+end
+
+put '/songs/:id' do
+  song = Song.get(params[:id])
+  song.update(params[:song])
+  redirect to("/songs/#{song.id}")
+end
+
+delete '/songs/:id' do
+  Song.get(params[:id]).destroy
+  redirect to('/songs')
 end
